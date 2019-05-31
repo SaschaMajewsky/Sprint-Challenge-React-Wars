c478dcaf59365f247138e98aea2ae26dfee159d4
Sascha <sascha.majewsky93@gmx.de>
2019-05-31 19:23:07 +0200

    added in image assets for pagination

--- starwars/src/components/StarWarsCharacterList.js
+++ starwars/src/components/StarWarsCharacterList.js
@@ -4,7 +4,12 @@ import StarWarsCharacter from './StarWarsCharacter'
 const StarWarsCharacterList = ({characterListProperty}) => (
         <div className='starWarsCharacterList'> 
             {characterListProperty.map(character => <StarWarsCharacter characterProperty={character} key={character.url}/> )}
+            <div className="pagination">
+                <div className="previous"></div>
+                <div className="next"></div>
+            </div>
         </div>
+
     )
 
 export default StarWarsCharacterList
