7c54943025a9747da36a362106f71f7a701e0c17
Sascha <sascha.majewsky93@gmx.de>
2019-05-31 18:09:51 +0200

    refactoring

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -17,13 +17,11 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
+    console.log(fetch(URL).then(res => res.json()));
     fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
+    .then(URL)
+      .then(res => res.json())
+      .then(data => this.setState({ starwarsChars: data.results }))
       .catch(err => {
         throw new Error(err);
       });
