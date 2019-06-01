78381ec69f4fea6eb11d6339d6e723c4b95939b8
Sascha <sascha.majewsky93@gmx.de>
2019-06-01 19:35:29 +0200

    refactored destructuring to best practise

--- starwars/src/components/StarWarsCharacter.js
+++ starwars/src/components/StarWarsCharacter.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './StarWars.css'
 
-const StarWarsCharacter = ({characterProperty}) => (
+const StarWarsCharacter = ({characterProperty: {name, gender, birth_year, height}}) => (
         <div className='starWarsCharacterDetailList'>
-            <div className='detail name'>{characterProperty.name} </div>
-            <div className='detail'> <span className="description">Sex:</span> {characterProperty.gender === "n/a" ? "robot" : characterProperty.gender} </div>
-            <div className='detail'> <span className="description">Birth:</span> {characterProperty.birth_year} </div>
-            <div className='detail'> <span className="description">Height:</span> {characterProperty.height / 100 + "m"} </div>
+            <div className='detail name'>{name} </div>
+            <div className='detail'> <span className="description">Sex:</span> {gender === "n/a" ? "robot" : gender} </div>
+            <div className='detail'> <span className="description">Birth:</span> {birth_year} </div>
+            <div className='detail'> <span className="description">Height:</span> {height / 100 + "m"} </div>
         </div>
     )
 
